3dc3cdb2c4bfacb992866fbf1f79f0ba3abc9861
Omprakash Choudhary <om78900987@gmail.com>
2022-05-31 18:29:05 +0530

    added basic layout

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,86 @@
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import TaskIcon from "@mui/icons-material/Task";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Synergy</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <DashboardIcon className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </Link>
+
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonOutlineIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <p className="title">TASK</p>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <TaskIcon className="icon" />
+              <span>Task</span>
+            </li>
+          </Link>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <TaskAltIcon className="icon" />
+              <span>Assigned Task</span>
+            </li>
+          </Link>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <li>
+            <ListAltIcon className="icon" />
+            <span>Request</span>
+          </li>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <li>
+              <CalendarMonthIcon className="icon" />
+              <span>Calender</span>
+            </li>
+          </Link>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
